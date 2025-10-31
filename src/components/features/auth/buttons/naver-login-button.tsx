1be55f056aff781b0144266aa7317a1833867891
sohxxny <sohxxny@gmail.com>
2025-10-31 18:22:05 +0900

    rename: 컴포넌트 분리 및 파일 이동 (#11)

---
+++ src/components/features/auth/buttons/naver-login-button.tsx
@@ -0,0 +1,6 @@
+import SocialLoginButton from './social-login-button';
+
+export default function NaverLoginButton() {
+  // onClick 수정
+  return <SocialLoginButton provider="naver" onClick={() => {}} />;
+}
---
+++ src/constants/auth.constants.ts
@@ -0,0 +1,12 @@
+export const PROVIDER_CONFIG = {
+  google: {
+    logo: '/auth/google-logo.png',
+    label: '구글 계정으로 계속하기',
+    alt: '구글 로고',
+  },
+  naver: {
+    logo: '/auth/naver-logo.png',
+    label: '네이버 계정으로 계속하기',
+    alt: '네이버 로고',
+  },
+} as const;
