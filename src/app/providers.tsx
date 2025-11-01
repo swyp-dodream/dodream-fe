33925f66f12e7475d5a919a3d7e1bc5e49b57130
sohxxny <sohxxny@gmail.com>
2025-11-01 21:16:12 +0900

    chore: Tanstack Query 설치 및 세팅 (#24)

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,15 +1,20 @@
 'use client';
 
 import { GoogleOAuthProvider } from '@react-oauth/google';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { OverlayProvider } from 'overlay-kit';
 import type React from 'react';
 
 export function Providers({ children }: { children: React.ReactNode }) {
+  const queryClient = new QueryClient();
+
   return (
-    <GoogleOAuthProvider
-      clientId={process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID ?? ''}
-    >
-      <OverlayProvider>{children}</OverlayProvider>
-    </GoogleOAuthProvider>
+    <QueryClientProvider client={queryClient}>
+      <GoogleOAuthProvider
+        clientId={process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID ?? ''}
+      >
+        <OverlayProvider>{children}</OverlayProvider>
+      </GoogleOAuthProvider>
+    </QueryClientProvider>
   );
 }
