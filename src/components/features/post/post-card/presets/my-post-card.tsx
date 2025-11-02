118e65f8fa8f777912187d91dfda56220b261852
mj0107 <alswns1285@gmail.com>
2025-11-03 00:49:56 +0900

    feat: '내가 쓴 글' 페이지에 들어가는 MyPostCard 컴포넌트 구현 (#28)

---
+++ src/components/features/post/post-card/presets/my-post-card.tsx
@@ -0,0 +1,54 @@
+import Button from '@/components/commons/buttons/button';
+import { PostCard } from '@/components/features/post/post-card';
+import type { MockPost, Role } from '@/mocks/posts';
+import { formatDeadlineAt } from '@/utils/date.util';
+
+const ROLE_LABEL_MAP: Record<Role, string> = {
+  FE: '프론트엔드',
+  BE: '백엔드',
+  iOS: 'iOS',
+  AOS: '안드로이드',
+  Designer: '디자이너',
+  PM: 'PM',
+  Planner: '기획자',
+  Marketer: '마케터',
+};
+
+function getRoles(roles: Role[]) {
+  return roles.map((role) => ROLE_LABEL_MAP[role]);
+}
+
+interface MyPostCardProps {
+  post: MockPost;
+}
+
+export default function MyPostCard({ post }: MyPostCardProps) {
+  return (
+    <PostCard>
+      <PostCard.Header
+        nickname={post.ownerUserId}
+        elapsedTime={formatDeadlineAt(post.deadlineAt)}
+        projectType={post.projectType}
+        isBookmarked={post.isBookmarked}
+        showBookmarkIcon={false}
+      />
+
+      <PostCard.Main>
+        <PostCard.Title>{post.title}</PostCard.Title>
+
+        <div className="flex flex-col gap-4">
+          <PostCard.TechCategories techCategories={post.techCategories} />
+          <PostCard.Roles roles={getRoles(post.roles)} />
+        </div>
+      </PostCard.Main>
+
+      <PostCard.Footer views={post.views} status={post.status} />
+
+      <PostCard.Actions>
+        <Button variant="solid" size="lg">
+          모집 내역
+        </Button>
+      </PostCard.Actions>
+    </PostCard>
+  );
+}
