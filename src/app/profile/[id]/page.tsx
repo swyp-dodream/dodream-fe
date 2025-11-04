5f3eb965e92fbb8be52d7d2ec5b94be300472bb6
sohxxny <sohxxny@gmail.com>
2025-11-05 00:50:24 +0900

    feat: 프로필 페이지에 목데이터 적용 (#20)

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -1,112 +1,87 @@
 import Image from 'next/image';
 import Link from 'next/link';
-import LinkIcon from '@/assets/icons/link/14.svg';
 import SuitcaseIcon from '@/assets/icons/suitcase/14.svg';
 import UsersIcon from '@/assets/icons/users/14.svg';
 import PostCardTechCategories from '@/components/features/post/post-card/post-card-tech-categories';
-import { TECH_CATEGORIES } from '@/mocks/posts';
-
-const SKILL_LIST = [
-  TECH_CATEGORIES[0],
-  TECH_CATEGORIES[1],
-  TECH_CATEGORIES[10],
-  TECH_CATEGORIES[13],
-];
+import InterestTags from '@/components/features/profile/interest-tags';
+import ProfileLinks from '@/components/features/profile/profile-link';
+import { PROFILE } from '@/mocks/profiles';
 
 export default function ProfilePage() {
+  // TODO: path 파라미로부터 프로필 데이터 페칭
+  const profile = PROFILE;
+
+  // TODO: 프로필 페이지 ID, 로그인 유저 ID 비교
+  const isMyProfile = true;
+
   return (
     <div className="h-full flex flex-col">
+      {/* 프로필 이미지/수정 버튼 */}
       <section className="flex justify-between">
+        {/* TODO: 이미지 수정 */}
         <Image
           src="/"
-          alt="사용자님의 프로필 이미지"
+          alt={`${profile.nickname}님의 프로필 이미지`}
           width={120}
           height={120}
           className="rounded-full bg-primary"
         />
-        <Link
-          href="/"
-          className="w-fit h-fit bg-primary px-5 py-3 rounded-full body-md-medium"
-          aria-label="프로필 정보 수정하기"
-        >
-          수정하기
-        </Link>
+        {isMyProfile && (
+          <Link
+            href="/"
+            className="w-fit h-fit bg-primary px-5 py-3 rounded-full body-md-medium"
+            aria-label="프로필 정보 수정하기"
+          >
+            수정하기
+          </Link>
+        )}
       </section>
       <div className="flex justify-between pt-5">
         <div className="w-[589px]">
+          {/* 닉네임 및 직무 */}
           <header>
-            <h2 className="heading-xl pb-3">닉네임입니다닉네임입</h2>
+            <h2 className="heading-xl pb-3">{profile.nickname}</h2>
             <ul className="flex flex-col gap-2 body-lg-medium">
               <li className="flex gap-3 items-center">
                 <SuitcaseIcon className="text-icon-medium" aria-hidden="true" />
                 <div className="flex gap-1 text-secondary">
-                  <div>디자이너</div>
+                  <div>{profile.roles}</div>
                   <div aria-hidden="true">·</div>
-                  <div>경력 1~3년</div>
+                  <div>{profile.experience}</div>
                 </div>
               </li>
               <li className="flex gap-3 items-center text-secondary">
                 <UsersIcon className="text-icon-medium" aria-hidden="true" />
-                <div>온라인 선호</div>
+                <div>{profile.activityMode} 선호</div>
               </li>
             </ul>
           </header>
+
+          {/* 자기소개 */}
           <section className="flex flex-col gap-4 pt-9 pb-8">
             <h3 className="heading-sm">자기소개</h3>
-            <p className="body-lg-regular">
-              안녕하세요, 직무 전문가 이름입니다. 간결한 한 줄 강점/경험을 통해
-              지원 분야에 기여하겠습니다. 첫째, 강점입니다. 관련 경험/성과를
-              통해 구체적 증명을 할 수 있었습니다. 구체적인 수치를 제시하여,
-              역량을 갖추었음을 증명했습니다. 둘째, 강점입니다. 관련 경험/성과를
-              통해 구체적 증명을 할 수 있었습니다. 구체적인 수치를 제시하여,
-              역량을 갖추었음을
-            </p>
+            <p className="body-lg-regular">{profile.introText}</p>
           </section>
+
+          {/* 링크 */}
           <section className="flex flex-col gap-4">
             <h3 className="heading-sm">링크</h3>
             <nav aria-label="사용자 외부 링크">
-              <ul className="flex flex-col gap-3 body-md-medium">
-                <li className="flex gap-3 items-center">
-                  <LinkIcon aria-hidden="true" />
-                  <Link href="/">
-                    https://notion.com/en/account/dhfjdfnkdvndnv
-                  </Link>
-                </li>
-                <li className="flex gap-3 items-center">
-                  <LinkIcon aria-hidden="true" />
-                  <Link href="/">
-                    https://notion.com/en/account/dhfjdfnkdvndnv
-                  </Link>
-                </li>
-                <li className="flex gap-3 items-center">
-                  <LinkIcon aria-hidden="true" />
-                  <Link href="/">
-                    https://notion.com/en/account/dhfjdfnkdvndnv
-                  </Link>
-                </li>
-              </ul>
+              <ProfileLinks profileUrls={profile.profileUrls} />
             </nav>
           </section>
         </div>
         <div className="w-[384px] flex flex-col">
+          {/* 기술 스택 */}
           <section className="flex flex-col gap-4">
             <h3 className="heading-sm">기술 스택</h3>
-            <PostCardTechCategories techCategories={SKILL_LIST} />
+            <PostCardTechCategories techCategories={profile.techSkills} />
           </section>
+
+          {/* 관심 분야 */}
           <section className="flex flex-col pt-8 pb-13 gap-4">
             <h3 className="heading-sm">관심 분야</h3>
-            <ul
-              className="flex gap-3 body-lg-regular"
-              aria-label="관심 분야 목록"
-            >
-              <li className="bg-primary py-2 px-4 rounded-full w-fit">환경</li>
-              <li className="bg-primary py-2 px-4 rounded-full w-fit">사회</li>
-              <li className="bg-primary py-2 px-4 rounded-full w-fit">
-                엔터테인먼트
-              </li>
-              <li className="bg-primary py-2 px-4 rounded-full w-fit">AI</li>
-              <li className="bg-primary py-2 px-4 rounded-full w-fit">금융</li>
-            </ul>
+            <InterestTags interests={profile.interestKeywords} />
           </section>
         </div>
       </div>
---
+++ src/mocks/profiles.ts
@@ -0,0 +1,15 @@
+export const PROFILE = {
+  nickname: '닉네임입니다닉네임입',
+  experience: '경력 1~3년',
+  activityMode: '온라인',
+  introText:
+    '안녕하세요, 직무 전문가 이름입니다. 간결한 한 줄 강점/경험을 통해 지원 분야에 기여하겠습니다. 첫째, 강점입니다. 관련 경험/성과를 통해 구체적으로 증명을 할 수 있었습니다. 구체적인 수치를 제시하여, 역량을 갖추었음을 증명했습니다. 둘째, 강점입니다. 관련 경험/성과를 통해 구체적 증명을 할 수 있었습니다. 구체적인 수치를 제시하여, 역량을 갖추었음을',
+  roles: '디자이너',
+  interestKeywords: ['환경', '사회', '엔터테인먼트', 'AI', '금융'],
+  techSkills: ['typeScript', 'javascript', 'nextjs', 'express', 'nodejs'],
+  profileUrls: [
+    'https://notion.com/en/account/dhfjdfnkdvndnv',
+    'https://github.com/djfhdsnf/',
+    'https://sjdnksndnv.com',
+  ],
+};
