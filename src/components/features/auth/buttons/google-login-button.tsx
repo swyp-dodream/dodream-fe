70ca6b28a324bc16b04efd34d6e97ea879c25226
sohxxny <sohxxny@gmail.com>
2025-11-04 15:27:45 +0900

    feat: 상단 네비게이션 로그인 여부 적용 (#24)

--- src/components/features/auth/buttons/google-login-button.tsx
+++ src/components/features/auth/buttons/google-login-button.tsx
@@ -2,6 +2,7 @@
 
 import { type TokenResponse, useGoogleLogin } from '@react-oauth/google';
 import authApi from '@/apis/auth.api';
+import { queryClient } from '@/lib/query-client';
 import { tokenStorage } from '@/utils/auth.util';
 import SocialLoginButton from './social-login-button';
 
@@ -22,6 +23,8 @@ export default function GoogleLoginButton({
     // 구글 Access Token 추출
     const accessToken = tokenResponse.access_token;
     tokenStorage.setToken(accessToken);
+    // TODO: 쿼리 무효화 인증 로직에 추가
+    await queryClient.invalidateQueries({ queryKey: ['user'] });
 
     onModalClose(); // 모달 닫기
   };
--- src/components/layout/navigation.tsx
+++ src/components/layout/navigation.tsx
@@ -6,6 +6,7 @@ import { overlay } from 'overlay-kit';
 import BellIcon from '@/assets/icons/bell/20.svg';
 import EditIcon from '@/assets/icons/edit/20.svg';
 import MessageCircleIcon from '@/assets/icons/message-circle/20.svg';
+import useGetUser from '@/hooks/auth/use-get-user';
 import Button from '../commons/buttons/button';
 import LoginModal from '../features/auth/login-modal';
 
@@ -13,10 +14,9 @@ import LoginModal from '../features/auth/login-modal';
  * 헤더의 네비게이션
  */
 export default function Navigation() {
-  // TODO: 로그인 여부 로직 변경
-  const isAuthenticated = false;
+  const { data: user } = useGetUser();
 
-  return isAuthenticated ? (
+  return user ? (
     // TODO: 네비게이션 링크 URL 변경
     // 로그인 상태: 네비게이션 바 노출
     <nav className="flex" aria-label="사용자 메뉴">
@@ -40,9 +40,14 @@ export default function Navigation() {
         </li>
 
         <li className="ml-2">
-          <Link href="/" aria-label="프로필" className="flex h-8 w-8 relative">
+          <Link
+            href="/"
+            aria-label="프로필"
+            className="flex h-8 w-8 relative rounded-full overflow-hidden"
+          >
+            {/* TODO: 프로필 이미지 컴포넌트 분리 */}
             <Image
-              src="/avatar/default-avatar.png"
+              src={user.picture ?? '/avatar/default-avatar.png'}
               alt="프로필 이미지"
               fill
               sizes="32px"
