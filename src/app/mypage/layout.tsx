ef33e022c13ab0408ccc346ab2a85b3b5c344e3e
sohxxny <sohxxny@gmail.com>
2025-11-05 00:12:24 +0900

    design: main에 1200px 제한 적용 (#20)

--- src/app/mypage/layout.tsx
+++ src/app/mypage/layout.tsx
@@ -4,7 +4,7 @@ export default function MyPageLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <div className="w-full h-full content-layout grid grid-cols-12 gap-7">
+    <div className="w-full h-full grid grid-cols-12 gap-7">
       <MyPageNavigation />
       <section className="col-span-8 col-start-4 flex flex-col gap-7">
         {children}
