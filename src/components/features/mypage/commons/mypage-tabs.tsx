57c2479f2785f98b5b1413c57f1175bc30228cde
mj0107 <alswns1285@gmail.com>
2025-11-05 15:21:52 +0900

    refactor: MyPageTabs.Content grid columns props로 받도록 수정

--- src/components/features/mypage/commons/mypage-tabs.tsx
+++ src/components/features/mypage/commons/mypage-tabs.tsx
@@ -43,11 +43,27 @@ function Trigger({
 
 function Content({
   className,
+  columns = 2,
   ...props
-}: ComponentPropsWithoutRef<typeof Tabs.Content>) {
+}: ComponentPropsWithoutRef<typeof Tabs.Content> & { columns?: number }) {
+  const GRID_COLUMNS: Record<number, string> = {
+    1: 'grid-cols-1',
+    2: 'grid-cols-2',
+    3: 'grid-cols-3',
+    4: 'grid-cols-4',
+    5: 'grid-cols-5',
+    6: 'grid-cols-6',
+    7: 'grid-cols-7',
+    8: 'grid-cols-8',
+    9: 'grid-cols-9',
+    10: 'grid-cols-10',
+    11: 'grid-cols-11',
+    12: 'grid-cols-12',
+  };
+
   return (
     <Tabs.Content
-      className={clsx('grid grid-cols-2 grid-rows-5 gap-7', className)}
+      className={clsx('grid gap-7', GRID_COLUMNS[columns], className)}
       {...props}
     />
   );
