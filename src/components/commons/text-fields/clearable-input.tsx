4d3d227ea727363633fe144dff1f04f7c75b8b52
sohxxny <sohxxny@gmail.com>
2025-11-05 17:20:46 +0900

    feat: 삭제/검색 버튼 추가한 input 컴포넌트 구현 (#31)

---
+++ src/components/commons/text-fields/clearable-input.tsx
@@ -0,0 +1,32 @@
+import clsx from 'clsx';
+import type { ComponentProps } from 'react';
+import XIcon from '@/assets/icons/x/14.svg';
+import Input from '@/components/commons/text-fields/input';
+
+interface ClearableInputProps extends ComponentProps<typeof Input> {
+  onClear: () => void;
+}
+
+/**
+ * 삭제 버튼이 있는 input 컴포넌트
+ * @param onClear - 삭제 버튼 핸들러
+ */
+export default function ClearableInput({
+  onClear,
+  className,
+  ...props
+}: ClearableInputProps) {
+  return (
+    <div className="relative">
+      <Input className={clsx('pr-[26px]', className)} {...props} />
+      <button
+        type="button"
+        onClick={onClear}
+        className="absolute right-3.5 top-1/2 -translate-y-1/2"
+        aria-label="삭제하기"
+      >
+        <XIcon className="text-icon-light" aria-hidden="true" />
+      </button>
+    </div>
+  );
+}
