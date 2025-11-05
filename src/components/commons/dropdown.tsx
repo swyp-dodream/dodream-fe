2e56afd99bd79133682ee27d06d1ac7cf1cb3c69
sohxxny <sohxxny@gmail.com>
2025-11-05 21:24:36 +0900

    feat: 커스텀 드롭다운 구현 (#31)

---
+++ src/components/commons/dropdown.tsx
@@ -0,0 +1,62 @@
+import clsx from 'clsx';
+import { DropdownMenu } from 'radix-ui';
+import ArrowIcon from '@/assets/icons/chevron-down/16.svg';
+
+interface DropdownItem {
+  label: string;
+  onClick?: () => void;
+  disabled?: boolean;
+}
+
+interface DropdownProps {
+  items: DropdownItem[];
+  className?: string;
+}
+
+/**
+ * 드롭다운 컴포넌트
+ * @param items - 드롭다운 아이템
+ * @example
+ * <Dropdown
+ *   items={[
+ *     { label: '프로필', onClick: () => console.log('프로필') },
+ *     { label: '설정', onClick: () => console.log('설정') },
+ *     { label: '로그아웃', onClick: () => console.log('로그아웃') },
+ *   ]}
+ * />
+ */
+export default function Dropdown({ items, className }: DropdownProps) {
+  return (
+    <DropdownMenu.Root>
+      <DropdownMenu.Trigger asChild>
+        <button
+          type="button"
+          className="py-3 px-4 body-lg-medium bg-container-primary rounded-md flex items-center justify-between w-[180px] outline-none group"
+        >
+          메뉴 열기
+          <ArrowIcon className="text-icon-light group-data-[state=open]:rotate-180" />
+        </button>
+      </DropdownMenu.Trigger>
+
+      <DropdownMenu.Portal>
+        <DropdownMenu.Content
+          className={clsx(
+            'bg-surface rounded-md shadow-card p-3 w-[180px] space-y-2 body-md-medium',
+            className,
+          )}
+        >
+          {items.map((item) => (
+            <DropdownMenu.Item
+              key={item.label}
+              onClick={item.onClick}
+              disabled={item.disabled}
+              className="px-2 py-2 cursor-pointer hover:bg-primary rounded outline-none"
+            >
+              {item.label}
+            </DropdownMenu.Item>
+          ))}
+        </DropdownMenu.Content>
+      </DropdownMenu.Portal>
+    </DropdownMenu.Root>
+  );
+}
