8ba8e70be2e772489a467261eb1e2f0975885460
mj0107 <alswns1285@gmail.com>
2025-11-06 17:00:43 +0900

    refactor: 마이페이지 헤더 모집내역에서는 밑 줄 없도록 수정 (#28)

--- src/components/features/mypage/commons/mypage-header.tsx
+++ src/components/features/mypage/commons/mypage-header.tsx
@@ -2,14 +2,20 @@ import { Separator } from 'radix-ui';
 
 interface MyPageHeaderProps {
   title: string;
+  isRecruitmentPage?: boolean;
 }
 
-export default function MyPageHeader({ title }: MyPageHeaderProps) {
+export default function MyPageHeader({
+  title,
+  isRecruitmentPage = false,
+}: MyPageHeaderProps) {
   return (
     <header className="flex flex-col gap-7">
       <h2 className="heading-xl">{title}</h2>
 
-      <Separator.Root className="w-full h-px bg-border-primary" />
+      {!isRecruitmentPage && (
+        <Separator.Root className="w-full h-px bg-border-primary" />
+      )}
     </header>
   );
 }
