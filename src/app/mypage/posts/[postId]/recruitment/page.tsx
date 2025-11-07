bd35790477af549dd5be8e072eeb6dd6d4b7d548
mj0107 <alswns1285@gmail.com>
2025-11-07 21:39:07 +0900

    feat: 모집 내역 페이지 UI 구현 (#28)

--- src/app/mypage/posts/[postId]/recruitment/page.tsx
+++ src/app/mypage/posts/[postId]/recruitment/page.tsx
@@ -1,3 +1,41 @@
+'use client';
+
+import MembersTabContent from '@/app/mypage/posts/[postId]/recruitment/components/members-tab-content';
+import MyPageHeader from '@/components/features/mypage/commons/mypage-header';
+import { RecruitmentTabs } from '@/components/features/mypage/my-posts/recruitments/recruitment-tabs';
+import ApplicantsTabContent from './components/applicants-tab-content';
+import OfferTabContent from './components/offer-tab-content';
+
+const RECRUITMENT_TABS = [
+  { tabValue: 'offers', label: '제안한 내역', Content: OfferTabContent },
+  {
+    tabValue: 'applicants',
+    label: '지원자 내역',
+    Content: ApplicantsTabContent,
+  },
+  { tabValue: 'members', label: '멤버 내역', Content: MembersTabContent },
+];
+
 export default function PostRecruitmentPage() {
-  <div>모집 내역</div>;
+  return (
+    <>
+      <MyPageHeader title="모집 내역" isRecruitmentPage />
+
+      <RecruitmentTabs defaultValue={RECRUITMENT_TABS[0].tabValue}>
+        <RecruitmentTabs.List>
+          {RECRUITMENT_TABS.map(({ tabValue, label }) => (
+            <RecruitmentTabs.Trigger key={tabValue} value={tabValue}>
+              {label}
+            </RecruitmentTabs.Trigger>
+          ))}
+        </RecruitmentTabs.List>
+
+        {RECRUITMENT_TABS.map(({ tabValue, Content }) => (
+          <RecruitmentTabs.Content key={tabValue} value={tabValue} columns={8}>
+            <Content />
+          </RecruitmentTabs.Content>
+        ))}
+      </RecruitmentTabs>
+    </>
+  );
 }
