8aac2d5d0dfc0b5081f2e97ac6b93927dc776fc2
sohxxny <sohxxny@gmail.com>
2025-11-08 05:35:04 +0900

    feat: 로그인 콜백 페이지 추가 (#39)

--- src/apis/auth.api.ts
+++ src/apis/auth.api.ts
@@ -1,9 +1,16 @@
 import api from '@/apis/api';
 import type { UserType } from '@/types/auth.type';
+import { tokenStorage } from '@/utils/auth.util';
 
 const authApi = {
-  // TODO: getUser 함수 수정
-  getUser: () => api.get<UserType>,
+  getUser: () => {
+    const token = tokenStorage.getToken();
+    return api.get<UserType>('/api/auth/me', {
+      headers: { Authorization: `Bearer ${token}` },
+    });
+    // TODO: 유저가 없는 경우에는 에러처리 하지 않고 null 반환
+    // TODO: 토큰 만료 시 에러처리 하지 않고 기존 토큰 삭제 및 토큰 재발급 시도
+  },
 };
 
 export default authApi;
