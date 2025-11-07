be213f79f12fd347aef068a6c67b166caab2064c
sohxxny <sohxxny@gmail.com>
2025-11-07 20:28:57 +0900

    feat: 홈 게시글 컴포넌트 구현 (#37)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Banner from '@/components/features/home/banner';
+import HomePosts from '@/components/features/home/home-posts';
 import RecommendedPosts from '@/components/features/home/recommended-posts';
 
 export default function Home() {
@@ -6,6 +7,7 @@ export default function Home() {
     <div className="grid grid-cols-12 gap-x-7 gap-y-12">
       <Banner />
       <RecommendedPosts />
+      <HomePosts />
     </div>
   );
 }
