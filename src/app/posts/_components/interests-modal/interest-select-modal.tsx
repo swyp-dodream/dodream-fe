4d0aeb3fb68b73d8a8b40737944ba347c3a81935
mj0107 <alswns1285@gmail.com>
2025-11-15 02:37:35 +0900

    feat: 기존 모집 직군 선택 modal 리팩토링 및 추가 구현 (#53)
    - 기존 string을 저장하던 형식에서 id를 저장하도록 리팩토링 및 추가 구현

---
+++ src/app/posts/_components/interests-modal/interest-select-modal.tsx
@@ -0,0 +1,102 @@
+import { useState } from 'react';
+import Button from '@/components/commons/buttons/button';
+import Modal from '@/components/commons/modal';
+import usePostCreateStore from '@/store/post-create-store';
+import type { InterestKeyword } from '@/types/interest.type';
+import InterestTabs from './interest-tabs';
+import InterestTags from './interest-tags';
+
+interface InterestSelectModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+}
+
+/**
+ * 관심분야 선택 모달
+ */
+export default function InterestSelectModal({
+  isOpen,
+  onClose,
+}: InterestSelectModalProps) {
+  const interests = usePostCreateStore((state) => state.interests);
+  const setInterests = usePostCreateStore((state) => state.setInterests);
+
+  const [draftInterests, setDraftInterests] =
+    useState<InterestKeyword[]>(interests);
+
+  /**
+   * 관심 분야 토글 함수
+   * @param interest - 관심 분야
+   */
+  const toggleInterests = (interest: InterestKeyword) => {
+    const exists = draftInterests.some(({ id }) => id === interest.id);
+
+    if (exists) {
+      setDraftInterests(draftInterests.filter(({ id }) => id !== interest.id));
+      return;
+    }
+
+    if (draftInterests.length >= 5) {
+      return;
+    }
+
+    setDraftInterests([...draftInterests, interest]);
+  };
+
+  // 저장 버튼 클릭 시 실제 관심분야 리스트 세팅
+  const handleSave = () => {
+    setInterests(draftInterests);
+    onClose();
+  };
+
+  return (
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <Modal.Overlay />
+      <Modal.Content
+        className="flex flex-col items-center py-5 px-7 h-[480px]"
+        size="lg"
+      >
+        <Modal.Title>관심 분야 선택</Modal.Title>
+        <Modal.Description>관심 분야를 선택하세요</Modal.Description>
+        <header>
+          <h3 className="body-lg-medium pb-5">관심 분야</h3>
+        </header>
+
+        {/* 관심 분야 선택 탭 */}
+        <InterestTabs
+          draftInterests={draftInterests}
+          toggleInterests={toggleInterests}
+        />
+
+        {/* 현재 선택된 태그 */}
+        <div className="py-6 mr-auto">
+          {draftInterests.length === 0 ? (
+            <span className="text-subtle body-md-regular">
+              가장 관심 있는 분야부터 순서대로 최대 5개까지 선택해주세요.
+            </span>
+          ) : (
+            <InterestTags
+              interests={draftInterests}
+              removeInterest={toggleInterests}
+            />
+          )}
+        </div>
+
+        {/* 저장 버튼 */}
+        <footer className="w-full flex justify-between items-center pt-4 border-t border-border-primary">
+          <span>{draftInterests.length}/5 선택됨</span>
+          {/* 아무것도 선택하지 않았을 경우 버튼 비활성화 */}
+          <Button
+            variant="solid"
+            size="xs"
+            onClick={handleSave}
+            disabled={draftInterests.length === 0}
+          >
+            저장
+          </Button>
+        </footer>
+        <Modal.Close />
+      </Modal.Content>
+    </Modal>
+  );
+}
---
+++ src/app/posts/_components/interests-modal/interest-tags.tsx
@@ -0,0 +1,75 @@
+import clsx from 'clsx';
+import XIcon from '@/assets/icons/x/12.svg';
+import { INDEX_LABEL } from '@/constants/profile.constant';
+import type { InterestKeyword } from '@/types/interest.type';
+
+interface InterestTagsProps {
+  interests: InterestKeyword[];
+  removeInterest: (interest: InterestKeyword) => void;
+  variant?: 'light' | 'dark';
+}
+
+/**
+ * 선택된 관심분야 리스트 컴포넌트
+ * @param draftInterests - 관심분야 리스트
+ * @param removeInterest- 관심분야 삭제 함수
+ * @param variant - 스타일
+ */
+export default function InterestTags({
+  interests,
+  removeInterest,
+  variant,
+}: InterestTagsProps) {
+  return (
+    <ul className="flex gap-4">
+      {interests.map((interest, index) => (
+        <InterestTag
+          key={interest.id}
+          index={index + 1}
+          variant={variant}
+          interest={interest}
+          onRemove={() => removeInterest(interest)}
+        />
+      ))}
+    </ul>
+  );
+}
+
+interface InterestTagProps {
+  interest: InterestKeyword;
+  index: number;
+  variant?: 'light' | 'dark';
+  onRemove: () => void;
+}
+
+/**
+ * 선택된 개별 관심분야 컴포넌트
+ * @param interest - 관심 분야
+ * @param index - 순서
+ * @param variant - 스타일
+ * @param onRemove - 선택 삭제 함수
+ */
+function InterestTag({
+  interest,
+  index,
+  variant = 'dark',
+  onRemove,
+}: InterestTagProps) {
+  return (
+    <li
+      className={clsx(
+        'flex gap-2 items-center px-4 py-3 rounded-full body-lg-medium',
+        {
+          'bg-button text-text-on-brand': variant === 'dark',
+          'bg-surface text-primary border border-border-primary':
+            variant === 'light',
+        },
+      )}
+    >
+      {`${INDEX_LABEL[index]} ${interest.name}`}
+      <button type="button" onClick={onRemove}>
+        <XIcon className="text-icon-light" />
+      </button>
+    </li>
+  );
+}
