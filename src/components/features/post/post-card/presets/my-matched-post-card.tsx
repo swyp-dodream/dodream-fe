41a01c8c4f9074ae0efc1d5285f27ba66c21951e
mj0107 <alswns1285@gmail.com>
2025-11-18 15:56:11 +0900

    feat: 매칭된 글 API 연동 (#83)

---
+++ src/components/features/post/post-card/presets/my-matched-post-card.tsx
@@ -0,0 +1,45 @@
+import { PostCard } from '@/components/features/post/post-card';
+import ApplyDetailButton from '@/components/features/post/post-card/buttons/apply-detail-button';
+import MathcingCancelButton from '@/components/features/post/post-card/buttons/matching-cancel-button';
+import type { MyMatchedPostType } from '@/types/post.type';
+import { formatDeadlineAt } from '@/utils/date.util';
+
+interface MatchedPostCardProps {
+  myMatchedPost: MyMatchedPostType;
+}
+
+export default function MyMatchedPostCard({
+  myMatchedPost,
+}: MatchedPostCardProps) {
+  return (
+    <PostCard href={`/post/${BigInt(myMatchedPost.postId)}`}>
+      <PostCard.Header
+        nickname={myMatchedPost.leaderName}
+        elapsedTime={formatDeadlineAt(myMatchedPost.matchedAt)}
+        projectType={myMatchedPost.projectType}
+        isBookmarked={myMatchedPost.bookmarked}
+      />
+
+      <PostCard.Main>
+        <PostCard.Title>{myMatchedPost.postTitle}</PostCard.Title>
+
+        <div className="flex flex-col gap-4">
+          <PostCard.TechCategories techCategories={myMatchedPost.stacks} />
+          <PostCard.Roles
+            roles={myMatchedPost.roles.map((role) => ({ role }))}
+          />
+        </div>
+      </PostCard.Main>
+
+      <PostCard.Footer
+        views={myMatchedPost.viewCount}
+        status={myMatchedPost.postStatus}
+      />
+
+      <PostCard.Actions>
+        <MathcingCancelButton nickname="닉네임" />
+        <ApplyDetailButton applicationId={myMatchedPost.id} />
+      </PostCard.Actions>
+    </PostCard>
+  );
+}
