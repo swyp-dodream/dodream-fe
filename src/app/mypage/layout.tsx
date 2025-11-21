72fac851b79defb596a86180882b2df9722d5c8b
sohxxny <sohxxny@gmail.com>
2025-11-21 17:52:34 +0900

    feat: 비로그인 상태로 마이페이지 접근 시 흠으로 리다이렉트 (#108)

--- src/app/mypage/layout.tsx
+++ src/app/mypage/layout.tsx
@@ -1,9 +1,44 @@
-import { Suspense } from 'react';
+'use client';
+
+import { useRouter } from 'next/navigation';
+import { Suspense, useEffect, useState } from 'react';
+import LoadingSpinner from '@/components/commons/loading-spinner';
 import MyPageNavigation from '@/components/layout/mypage-navigation';
+import { useGetProfile } from '@/hooks/profile/use-get-profile';
+import useToast from '@/hooks/use-toast';
 
 export default function MyPageLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
+  const { data: profile, isLoading } = useGetProfile();
+  const [mounted, setMounted] = useState(false);
+  const toast = useToast();
+  const router = useRouter();
+
+  // 마운트 확인
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  useEffect(() => {
+    if (mounted && !isLoading && !profile) {
+      toast({ title: '로그인이 필요합니다.' });
+      router.replace('/');
+    }
+  }, [profile, isLoading, mounted, router, toast]);
+
+  if (!mounted || isLoading) {
+    return (
+      <div className="w-full h-full flex items-center justify-center">
+        <LoadingSpinner variant="lg" />
+      </div>
+    );
+  }
+
+  if (!profile) {
+    return null;
+  }
+
   return (
     <div className="w-full h-full grid grid-cols-12 gap-7">
       <MyPageNavigation />
