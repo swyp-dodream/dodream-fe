6243b6df5e2c7c35a36ed8c6f40c1b1d60db6009
sohxxny <sohxxny@gmail.com>
2025-11-23 00:33:14 +0900

    feat: 툴팁 추가 (#68)

--- src/app/post/_components/recommended-users.tsx
+++ src/app/post/_components/recommended-users.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import OfferTabContent from '@/app/mypage/posts/[postId]/recruitment/_components/offer-tab-content';
+import DefaultTooltip from '@/components/commons/tooltip/default-tooltip';
 import { useGetRecommendedUsers } from '@/hooks/post/use-get-recommended-users';
 
 interface RecommendedUsersProps {
@@ -20,7 +21,14 @@ export default function RecommendedUsers({ postId }: RecommendedUsersProps) {
 
   return (
     <div className="flex flex-col gap-8">
-      <h3 className="heading-lg">AI가 추천하는 회원을 탐색해 보세요</h3>
+      <div className="flex gap-3 items-center">
+        <h3 className="heading-lg">AI가 추천하는 회원을 탐색해 보세요</h3>
+        <DefaultTooltip
+          content="회원님이 게시한 모집글 정보를 바탕으로 
+  가장 잘 맞을 지원자를 꼽아 추천해요.
+  목록에서 프로필을 확인하고 바로 합류 제안을 보내보세요."
+        />
+      </div>
       <OfferTabContent postId={postId} users={profiles} />
     </div>
   );
