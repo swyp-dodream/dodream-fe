c8fbcde7b160d1e1f47757f20f231f2a0f02e542
mj0107 <alswns1285@gmail.com>
2025-11-23 02:12:02 +0900

    design: 채팅 페이지 레이아웃 수정 (#104)

--- src/app/chat/layout.tsx
+++ src/app/chat/layout.tsx
@@ -1,5 +1,7 @@
 export default function ChatPageLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
-  return <div className="w-full h-full grid grid-cols-12">{children}</div>;
+  return (
+    <div className="w-full h-full grid grid-cols-12 gap-7">{children}</div>
+  );
 }
