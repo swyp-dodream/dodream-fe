49a71193bff1d74bc95aded78591341e176a2a21
mj0107 <alswns1285@gmail.com>
2025-11-23 17:11:12 +0900

    feat: 채팅방 메시지 타입 선언 (#104)

--- src/types/chat.type.ts
+++ src/types/chat.type.ts
@@ -31,7 +31,26 @@ export type ChatListItemType = {
   leaderId: string;
   memberId: string;
   myRole: string;
+  lastMessage: string;
+  lastMessageAt: Date;
 };
 
 /** 내 채팅방 목록 조회 응답 타입 */
 export type GetChatListResponseType = ChatListItemType[];
+
+/** 구독한 채팅방 메시지 응답 타입 */
+export type ChatSubscribeMessageType = {
+  body: string;
+  createdAt: string;
+  id: string;
+  messageType: string;
+  postId: string;
+  receiverId: string;
+  roomId: string;
+  senderId: string;
+};
+
+/** 채팅방에서 사용할 메시지 타입 */
+export type GroupedChatType = ChatSubscribeMessageType & {
+  isMyMessage: boolean;
+};
