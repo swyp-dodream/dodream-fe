c9df8e14220de0a4eafdbc561bea5c175000410d
sohxxny <sohxxny@gmail.com>
2025-11-23 18:40:22 +0900

    refactor: AI 추천 회원 탭 제안 탭과 분리 (#112)

--- src/app/post/_components/recommended-users.tsx
+++ src/app/post/_components/recommended-users.tsx
@@ -1,7 +1,10 @@
 'use client';
 
-import OfferTabContent from '@/app/mypage/posts/[postId]/recruitment/_components/offer-tab-content';
 import DefaultTooltip from '@/components/commons/tooltip/default-tooltip';
+import OfferButton from '@/components/features/mypage/my-posts/recruitments/buttons/offer-button';
+import RecruitmentUserRow from '@/components/features/mypage/my-posts/recruitments/recruitment-user-row';
+import { RoleTabs } from '@/components/features/mypage/my-posts/recruitments/role-tabs';
+import UserActions from '@/components/features/mypage/my-posts/recruitments/user-actions';
 import { useGetRecommendedUsers } from '@/hooks/post/use-get-recommended-users';
 
 interface RecommendedUsersProps {
@@ -14,10 +17,15 @@ export default function RecommendedUsers({ postId }: RecommendedUsersProps) {
 
   if (!data) return null;
 
-  const profiles = data.profiles.map((profile) => ({
+  const users = data.profiles.map((profile) => ({
     ...profile,
     jobGroups: profile.roles,
   }));
+
+  // users에 존재하는 역할만 추출
+  const availableRoles = Array.from(
+    new Set(users.map((user) => user.jobGroups[0])),
+  );
 
   return (
     <div className="flex flex-col gap-8">
@@ -29,7 +37,37 @@ export default function RecommendedUsers({ postId }: RecommendedUsersProps) {
   목록에서 프로필을 확인하고 바로 합류 제안을 보내보세요."
         />
       </div>
-      <OfferTabContent postId={postId} users={profiles} />
+      <RoleTabs defaultValue={availableRoles[0]}>
+        {/* TODO: 탭 분리 */}
+        <RoleTabs.List>
+          {availableRoles.map((role) => (
+            <RoleTabs.Trigger key={role} value={role}>
+              {role}
+            </RoleTabs.Trigger>
+          ))}
+        </RoleTabs.List>
+
+        {availableRoles.map((role) => (
+          <RoleTabs.Content key={role} value={role} columns={8}>
+            <div className="grid grid-cols-subgrid col-span-full gap-6 divide-y divide-border-primary">
+              {users
+                .filter(({ jobGroups }) => jobGroups[0] === role)
+                .map((user) => (
+                  <RecruitmentUserRow
+                    postId={BigInt(postId)}
+                    key={user.userId}
+                    {...user}
+                    actions={
+                      <UserActions>
+                        <OfferButton postId={postId} userId={user.userId} />
+                      </UserActions>
+                    }
+                  />
+                ))}
+            </div>
+          </RoleTabs.Content>
+        ))}
+      </RoleTabs>
     </div>
   );
 }
