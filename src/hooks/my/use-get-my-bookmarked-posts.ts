e34ad952871e99b273c9e664e857015dff5d4c88
mj0107 <alswns1285@gmail.com>
2025-11-24 17:30:50 +0900

    refactor: 북마크 API 수정으로 인한 컴포넌트 수정 (#105)

--- src/hooks/my/use-get-my-bookmarked-posts.ts
+++ src/hooks/my/use-get-my-bookmarked-posts.ts
@@ -1,12 +1,17 @@
 import { useQuery } from '@tanstack/react-query';
 import postApi from '@/apis/post.api';
 import { QUERY_KEY } from '@/constants/query-key.constant';
+import type { ProjectType } from '@/types/post.type';
 import { tokenStorage } from '@/utils/auth.util';
 
-export default function useGetMyBookmarkedPosts(page?: number, size?: number) {
+export default function useGetMyBookmarkedPosts(
+  projectType: ProjectType,
+  page?: number,
+  size?: number,
+) {
   return useQuery({
-    queryKey: [QUERY_KEY.auth, QUERY_KEY.myBookmarkedPosts],
-    queryFn: () => postApi.getMyBookmarkedPosts(page, size),
-    enabled: tokenStorage.hasToken,
+    queryKey: [QUERY_KEY.auth, QUERY_KEY.myBookmarkedPosts, projectType],
+    queryFn: () => postApi.getMyBookmarkedPosts(projectType, page, size),
+    enabled: tokenStorage.hasToken(),
   });
 }
