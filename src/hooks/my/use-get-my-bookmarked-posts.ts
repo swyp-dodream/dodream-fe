40be0c83197657e62ba4ee135ab772522b7c9441
sohxxny <sohxxny@gmail.com>
2025-11-24 17:08:29 +0900

    feat: 게시물 리스트/상세 페이지에서 북마크 여부 연동 (#114)

--- src/hooks/my/use-get-my-bookmarked-posts.ts
+++ src/hooks/my/use-get-my-bookmarked-posts.ts
@@ -1,12 +1,14 @@
 import { useQuery } from '@tanstack/react-query';
 import postApi from '@/apis/post.api';
 import { QUERY_KEY } from '@/constants/query-key.constant';
-import { tokenStorage } from '@/utils/auth.util';
+import { useGetProfileExists } from '../profile/use-get-profile';
 
 export default function useGetMyBookmarkedPosts(page?: number, size?: number) {
+  const { data: profileExists, isSuccess } = useGetProfileExists();
+
   return useQuery({
     queryKey: [QUERY_KEY.auth, QUERY_KEY.myBookmarkedPosts],
     queryFn: () => postApi.getMyBookmarkedPosts(page, size),
-    enabled: tokenStorage.hasToken,
+    enabled: isSuccess && profileExists.exists === true,
   });
 }
