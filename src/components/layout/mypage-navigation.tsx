3a5d288c39eac054397c063bd9344c09c8cd7930
mj0107 <alswns1285@gmail.com>
2025-11-28 01:43:56 +0900

    feat: 마이페이지 네비게이션에서 로그아웃 구현

--- src/components/layout/mypage-navigation.tsx
+++ src/components/layout/mypage-navigation.tsx
@@ -3,6 +3,7 @@
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import { MYPAGE_MENU_LIST } from '@/constants/menus/mypage';
+import { logout } from '@/utils/auth.util';
 
 export default function MyPageNavigation() {
   const pathname = usePathname();
@@ -13,25 +14,36 @@ export default function MyPageNavigation() {
       className="col-span-2 h-full border-r-1 border-border-primary"
     >
       <ul className="flex flex-col gap-7 sticky top-32">
-        {MYPAGE_MENU_LIST.map(({ label, href, icon }) => {
-          const isActive = pathname.startsWith(href);
+        {MYPAGE_MENU_LIST.map(({ label, href, icon, type = 'link' }) => {
+          const isActive = type === 'link' && pathname.startsWith(href);
           const Icon = icon.default;
 
           return (
-            <li key={href}>
-              <Link
-                aria-current={isActive ? 'page' : undefined}
-                className="flex items-center gap-4 heading-md"
-                href={href}
-              >
-                <Icon
-                  aria-hidden
-                  className={isActive ? 'text-icon-dark' : 'text-icon-light'}
-                />
-                <span className={isActive ? 'text-primary' : 'text-subtle'}>
-                  {label}
-                </span>
-              </Link>
+            <li key={label}>
+              {type === 'button' ? (
+                <button
+                  type="button"
+                  className="flex items-center gap-4 heading-md text-subtle"
+                  onClick={() => logout()}
+                >
+                  <Icon aria-hidden className="text-icon-light" />
+                  <span>{label}</span>
+                </button>
+              ) : (
+                <Link
+                  aria-current={isActive ? 'page' : undefined}
+                  className="flex items-center gap-4 heading-md"
+                  href={href}
+                >
+                  <Icon
+                    aria-hidden
+                    className={isActive ? 'text-icon-dark' : 'text-icon-light'}
+                  />
+                  <span className={isActive ? 'text-primary' : 'text-subtle'}>
+                    {label}
+                  </span>
+                </Link>
+              )}
             </li>
           );
         })}
