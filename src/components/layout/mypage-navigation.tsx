e62f30f244c5b263aaa3dc6441681c4a88ac7d22
mj0107 <alswns1285@gmail.com>
2025-11-28 01:18:48 +0900

    fix: 마이페이지 네이게이션 겹침 문제 해결 (#128)

--- src/components/layout/mypage-navigation.tsx
+++ src/components/layout/mypage-navigation.tsx
@@ -12,7 +12,7 @@ export default function MyPageNavigation() {
       aria-label="마이페이지 메뉴"
       className="col-span-2 h-full border-r-1 border-border-primary"
     >
-      <ul className="flex flex-col gap-7 fixed">
+      <ul className="flex flex-col gap-7 sticky top-32">
         {MYPAGE_MENU_LIST.map(({ label, href, icon }) => {
           const isActive = pathname.startsWith(href);
           const Icon = icon.default;
