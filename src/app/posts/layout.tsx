3b5320cc166c48153888e95344e09bf13e05ff22
mj0107 <alswns1285@gmail.com>
2025-11-28 15:51:11 +0900

    design: 모집글 생성/수정 레이아웃 공통화 (#141)

--- src/app/posts/layout.tsx
+++ src/app/posts/layout.tsx
@@ -1,4 +1,4 @@
-export default function PostCreatePageLayout({
+export default function PostsPageLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return <div className="w-full flex justify-center">{children}</div>;
