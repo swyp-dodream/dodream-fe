8f0f9c51ae74e67a16d7f3aa9b83a66f08b8bad5
sohxxny <sohxxny@gmail.com>
2026-02-05 22:56:58 +0900

    refactor: 모집글 작성의 기술스택 모달 기존 컴포넌트로 대체 (#169)

--- src/app/(header-only)/posts/_components/fields/tech-stacks-field.tsx
+++ src/app/(header-only)/posts/_components/fields/tech-stacks-field.tsx
@@ -3,44 +3,34 @@
 import { overlay } from 'overlay-kit';
 import { useEffect } from 'react';
 import { useFormContext } from 'react-hook-form';
+import TechStackSelectModal from '@/app/(header-only)/create-profile/_components/tech-stack-modal/tech-stack-select-modal';
+import TechStackTags from '@/app/(header-only)/create-profile/_components/tech-stack-modal/tech-stack-tags';
 import FieldErrorMessage from '@/app/(header-only)/posts/_components/field-error-message';
-import TechStackSelectModal from '@/app/(header-only)/posts/_components/tech-stack-modal/tech-stack-select-modal';
-import TechStackTags from '@/app/(header-only)/posts/_components/tech-stack-modal/tech-stack-tags';
 import ArrowIcon from '@/assets/icons/chevron-down/16.svg';
 import DropdownButton from '@/components/commons/buttons/dropdown-button';
 import type { PostCreateFormData } from '@/schemas/post.schema';
-import usePostCreateStore from '@/store/post-create-store';
 
 export default function TechStacksField() {
   const {
+    watch,
     register,
     setValue,
-    getValues,
     formState: { errors },
   } = useFormContext<PostCreateFormData>();
-  const techStacks = usePostCreateStore((state) => state.techStacks);
-  const removeStacks = usePostCreateStore((state) => state.removeStacks);
+
+  const stackIds = watch('stackIds');
 
   useEffect(() => {
     register('stackIds');
   }, [register]);
 
-  useEffect(() => {
-    const prevIds = getValues('stackIds');
-    const nextIds = techStacks.map((techStack) => techStack.id);
-    const hasChanged =
-      prevIds.length !== nextIds.length ||
-      prevIds.some((id, index) => id !== nextIds[index]);
-
-    if (!hasChanged) {
-      return;
-    }
-
-    setValue('stackIds', nextIds, {
-      shouldDirty: true,
-      shouldValidate: true,
-    });
-  }, [getValues, setValue, techStacks]);
+  const removeStack = (stackId: number) => {
+    setValue(
+      'stackIds',
+      stackIds.filter((id) => id !== stackId),
+      { shouldDirty: true, shouldValidate: true },
+    );
+  };
 
   return (
     <div>
@@ -54,7 +44,17 @@ export default function TechStacksField() {
             label="기술 스택 선택"
             onClick={() => {
               overlay.open(({ isOpen, close }) => (
-                <TechStackSelectModal isOpen={isOpen} onClose={close} />
+                <TechStackSelectModal
+                  isOpen={isOpen}
+                  onClose={close}
+                  initialStacks={stackIds}
+                  onSave={(newStackIds) => {
+                    setValue('stackIds', newStackIds, {
+                      shouldDirty: true,
+                      shouldValidate: true,
+                    });
+                  }}
+                />
               ));
             }}
             isError={!!errors.stackIds}
@@ -63,12 +63,12 @@ export default function TechStacksField() {
           </DropdownButton>
         </div>
 
-        {techStacks.length > 0 && (
+        {stackIds.length > 0 && (
           <div className="ml-auto mt-4">
             <TechStackTags
               variant="md"
-              stacks={techStacks}
-              removeStacks={removeStacks}
+              stacks={stackIds}
+              removeStacks={removeStack}
             />
           </div>
         )}
