b6ac523dd8194dcd88130e3ee7abbaebed5d0346
sohxxny <sohxxny@gmail.com>
2026-02-18 21:30:46 +0900

    feat: 배너의 로그인 여부를 서버함수로 판단 (#171)

--- src/app/(default-layout)/(home)/_components/banner.tsx
+++ src/app/(default-layout)/(home)/_components/banner.tsx
@@ -1,18 +1,9 @@
-'use client';
-
 import Link from 'next/link';
-import { overlay } from 'overlay-kit';
-import LoginModal from '@/app/auth/_components/login-modal';
-import { useGetProfileExists } from '@/hooks/profile/use-get-profile';
+import { serverApis } from '@/services/server.api';
+import BannerLoginButton from './banner-login-button';
 
-export default function Banner() {
-  const { data: profileExists } = useGetProfileExists();
-
-  const handleLogin = () => {
-    overlay.open(({ isOpen, close }) => (
-      <LoginModal isOpen={isOpen} onClose={close} />
-    ));
-  };
+export default async function Banner() {
+  const profileExists = await serverApis.profile.getProfileExists();
 
   const bgImage =
     profileExists?.exists === true
@@ -21,11 +12,11 @@ export default function Banner() {
 
   return (
     <aside
-      className={`col-span-12 flex flex-col justify-between relative h-[204px] shadow-card bg-white border border-border-primary rounded-lg overflow-hidden ${bgImage} bg-cover`}
+      className={`col-span-12 flex flex-col justify-between relative h-51 shadow-card bg-white border border-border-primary rounded-lg overflow-hidden ${bgImage} bg-cover`}
       aria-labelledby="banner-heading"
     >
       <div className="mt-8 ml-9">
-        <h2 className="heading-lg w-[326px]" id="banner-heading">
+        <h2 className="heading-lg w-81.5" id="banner-heading">
           {profileExists
             ? '모집글을 작성하고 모집글과 꼭 맞는 AI 지원자 추천을 경험해 보세요.'
             : '놓치고 있는 기회가 있을지 몰라요!'}
@@ -49,13 +40,7 @@ export default function Banner() {
           모집글 작성하기
         </Link>
       ) : (
-        <button
-          type="button"
-          onClick={handleLogin}
-          className="bg-brand text-text-on-brand mb-8 ml-9 rounded-md w-fit px-4 py-2 body-md-medium"
-        >
-          로그인하고 AI 추천 받아보기
-        </button>
+        <BannerLoginButton />
       )}
     </aside>
   );
