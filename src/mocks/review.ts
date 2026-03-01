7cabbe7df7a97a92c44832c892945970a63cb79c
sohxxny <sohxxny@gmail.com>
2026-03-01 16:30:39 +0900

    feat: 멤버 내역 목데이터 추가 (#176)

---
+++ src/mocks/review.ts
@@ -0,0 +1,7 @@
+export const members = [
+  { id: '1', nickname: '유저1' },
+  { id: '2', nickname: '유저2' },
+  { id: '3', nickname: '유저3' },
+  { id: '4', nickname: '유저4' },
+  { id: '5', nickname: '유저5' },
+];
