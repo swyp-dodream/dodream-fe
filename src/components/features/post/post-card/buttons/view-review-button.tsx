dc866ecac2bd3002b652834db01222b481ffd02b
sohxxny <sohxxny@gmail.com>
2026-03-02 01:03:32 +0900

    feat: 리뷰 확인 모달 구현 (#176)

--- src/components/features/post/post-card/buttons/view-review-button.tsx
+++ src/components/features/post/post-card/buttons/view-review-button.tsx
@@ -1,8 +1,16 @@
+import { overlay } from 'overlay-kit';
 import Button from '@/components/commons/buttons/button';
+import ViewReviewModal from '@/components/features/reviews/view-review-modal';
 
 export default function ViewReviewButton() {
+  const handleOpenModal = () => {
+    overlay.open(({ isOpen, close }) => (
+      <ViewReviewModal isOpen={isOpen} onClose={close} />
+    ));
+  };
+
   return (
-    <Button variant="outline" size="md">
+    <Button variant="outline" size="md" onClick={handleOpenModal}>
       후기 확인
     </Button>
   );
---
+++ src/components/features/reviews/view-review-modal.tsx
@@ -0,0 +1,127 @@
+import ThumbsDownIcon from '@/assets/icons/thumbs-down/14.svg';
+import ThumbsUpIcon from '@/assets/icons/thumbs-up/14.svg';
+import Button from '@/components/commons/buttons/button';
+import Modal from '@/components/commons/modal';
+import { REVIEW_ICONS, REVIEW_TAG_LABEL } from '@/constants/review.constant';
+import { useGetProfile } from '@/hooks/profile/use-get-profile';
+import { reviews } from '@/mocks/review.mock';
+import type { Reaction, ReviewTag } from '@/types/review.type';
+
+interface ViewReviewModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+}
+
+/** 리뷰 확인 모달 */
+export default function ViewReviewModal({
+  isOpen,
+  onClose,
+}: ViewReviewModalProps) {
+  const { data: profile } = useGetProfile();
+
+  // 긍정 후기 수
+  const positiveCount = reviews.filter(
+    (review) => review.feedbackType === 'positive',
+  ).length;
+
+  // 부정 후기 수
+  const negativeCount = reviews.filter(
+    (review) => review.feedbackType === 'negative',
+  ).length;
+
+  // 어떤 타입 보여줘야 하는지 확인
+  const dominantType: Reaction =
+    positiveCount >= negativeCount ? 'positive' : 'negative';
+
+  // 각 태그 개수 카운팅
+  const sortedTagCounts = reviews
+    .filter((review) => review.feedbackType === dominantType)
+    .flatMap((review) => review.options)
+    .reduce(
+      (acc, tag) => {
+        acc[tag] = (acc[tag] ?? 0) + 1;
+        return acc;
+      },
+      {} as Record<ReviewTag, number>,
+    );
+
+  // 태그 많은 순으로 정렬
+  const result = Object.entries(sortedTagCounts)
+    .sort(([, a], [, b]) => b - a)
+    .map(([tag, count]) => ({ tag: tag as ReviewTag, count }));
+
+  return (
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <Modal.Overlay />
+      <Modal.Content className="flex flex-col py-5 px-7" size="lg">
+        <header className="flex items-start justify-between">
+          <Modal.Title>후기 확인</Modal.Title>
+          <Modal.Close />
+          <p
+            aria-hidden="true"
+            className="body-lg-medium w-full text-center mb-5"
+          >
+            후기 확인
+          </p>
+        </header>
+
+        {/* 리뷰 요약 */}
+        <section className="flex flex-col gap-2 mb-9">
+          <p>{profile?.nickname}님의 협업 점수는?</p>
+          <div className="flex items-center gap-2">
+            {dominantType === 'positive' ? (
+              <>
+                <ThumbsUpIcon />
+                <p className="body-lg-medium">
+                  {reviews.length}명 중 {positiveCount}명이 {profile?.nickname}
+                  님과의 함께한 경험이 좋았다고 말했어요
+                </p>
+              </>
+            ) : (
+              <>
+                <ThumbsDownIcon />
+                <p className="body-lg-medium">
+                  {reviews.length}명 중 {negativeCount}명이 {profile?.nickname}
+                  님과의 함께한 경험이 좋았다고 말했어요
+                </p>
+              </>
+            )}
+          </div>
+        </section>
+
+        {/* 받은 상세 후기 */}
+        <section className="flex flex-col gap-4 mb-5">
+          <p className="body-md-medium">
+            {profile?.nickname}님이 받은 상세 후기예요
+          </p>
+          <ul className="grid grid-cols-2 gap-y-3">
+            {result.map((tag) => {
+              const Icon = REVIEW_ICONS[tag.tag];
+
+              return (
+                <li
+                  key={tag.tag}
+                  className="flex items-center mx-3 bg-primary py-4 px-5 rounded-md justify-between"
+                >
+                  <div className="flex items-center gap-2">
+                    <Icon aria-hidden="true" className="text-primary" />
+                    <span className="body-md-regular">
+                      {REVIEW_TAG_LABEL[tag.tag]}
+                    </span>
+                  </div>
+                  {tag.count}
+                </li>
+              );
+            })}
+          </ul>
+        </section>
+
+        <footer className="flex justify-end gap-5 border-t-1 border-border-primary pt-4">
+          <Button variant="solid" onClick={onClose}>
+            확인
+          </Button>
+        </footer>
+      </Modal.Content>
+    </Modal>
+  );
+}
