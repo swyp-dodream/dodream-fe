8928412814e3e8ee57c26e7d4bcf4a2dfecf65cd
sohxxny <sohxxny@gmail.com>
2026-03-10 20:54:17 +0900

    fix: BigInt 타입 ID 잘못 전달되는 문제 해결 (#179)

--- src/hooks/review/use-create-review.ts
+++ src/hooks/review/use-create-review.ts
@@ -18,8 +18,8 @@ export default function useCreateReview() {
           .filter((r) => r.reaction !== null)
           .map((review) =>
             clientApis.review.createReviews({
-              postId: postId.toString(),
-              toUserId: review.userId.toString(),
+              postId: BigInt(postId).toString(),
+              toUserId: BigInt(review.userId).toString(),
               feedbackType: review.reaction!,
               options: review.tags,
             }),
